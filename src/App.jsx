206a03b8559d423d5932a65571bb6dcb9b6c187e
Sebatian <Sxe1exs@gmail.com>
2023-12-08 13:28:16 -0700

    Addition of NameBadge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
+import NameBadge from './components/NameBadge/NameBadge';
 
 function App() {
   return (
     <div className="App">
-
+      <NameBadge/>
     </div>
   );
 }
---
+++ src/components/NameBadge/NameBadge.jsx
@@ -0,0 +1,7 @@
+const NameBadge = ({}) => {
+    return ( 
+    <h1>Sebastian</h1> 
+    );
+}
+ 
+export default NameBadge;
